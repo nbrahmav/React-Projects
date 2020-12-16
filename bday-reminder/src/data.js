2bac39f292a08183718c0e5e66f0435711889967
Nivedi B <nbrahmav@gmail.com>
2020-12-16 10:22:14 +0300

    Bday Reminder

---
+++ bday-reminder/src/data.js
@@ -0,0 +1,33 @@
+export default[
+    {
+        id:1,
+        name: 'Simone Rodrigues',
+        age:29,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+    },
+
+    {
+        id: 2,
+        name: 'Mike Roland',
+        age: 45,
+        image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg'
+
+    },
+    {
+        id: 3,
+        name: 'Rnoit',
+        age: 34,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg'
+
+    },
+    {
+        id:4,
+        name: 'Allena Rodrigues',
+        age:29,
+        image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
+    },
+];
